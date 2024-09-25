df91210181497778dad29c957ac1f69060d2a875
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-09-26 01:51:42 +1000

    Test using "Quit To Menu" as clone base
    Pause menu still appears...

--- src/Tweaks/QuitToLobbyButton.cs
+++ src/Tweaks/QuitToLobbyButton.cs
@@ -45,12 +45,27 @@ namespace ServerSider
 
         private static void PauseScreenController_Awake(PauseScreenController __instance)
         {
+            HGButton[] candidates = __instance.exitGameButton.gameObject.transform.parent.GetComponentsInChildren<HGButton>();
+            for (int i = 0; i < candidates.Length; i++) {
+                // Look for "Quit to Menu" button
+                Plugin.Logger.LogWarning(candidates[i].name);
+                if (candidates[i].onClick.m_PersistentCalls.GetListener(0).arguments.stringArgument == "quit_confirmed_command \"transition_command disconnect;\"") {
+                    CreateButton(candidates[i]);
+                    return;
+                }
+            }
+            Plugin.Logger.LogWarning($"{nameof(QuitToLobbyButton)}> Failed to create button (could not find base to clone).");
+        }
+
+        private static void CreateButton(HGButton cloneBase)
+        {
             const string label = "Quit to Lobby"; // Should capitalise "to" to match existing buttons, but I prefer title case
-            GameObject obj = Object.Instantiate(__instance.exitGameButton, __instance.exitGameButton.transform.parent);
+            GameObject obj = Object.Instantiate(cloneBase.gameObject, cloneBase.transform.parent);
             Object.DestroyImmediate(obj.GetComponent<LanguageTextMeshController>()); // Will override text (e.g. when returning from a submenu)
             obj.name = $"GenericMenuButton ({label})";
             obj.GetComponentInChildren<HGTextMeshProUGUI>().text = label;
             HGButton btn = obj.GetComponent<HGButton>();
+            // "Quit to Desktop" button (exitGameButton)
             // [0] SubmitCmd | OptionsPanel(JUICED)(RoR2.ConsoleFunctions)
             //     quit_confirmed_command "quit"
             btn.onClick.m_PersistentCalls.GetListener(0).arguments.stringArgument = "quit_confirmed_command \"run_end\"";
