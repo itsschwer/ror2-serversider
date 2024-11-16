bddab1b7496c5f4a5d872052b4d8edf9911fd39f
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-11-16 21:39:37 +1100

    Adjust TeleportOutOfBoundsPickups logic

--- src/Tweaks/TeleportOutOfBoundsPickups.cs
+++ src/Tweaks/TeleportOutOfBoundsPickups.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx.Configuration;
 using RoR2;
+using System.Text;
 using UnityEngine;
 
 namespace ServerSider
@@ -73,49 +74,83 @@ namespace ServerSider
             orig(self, other);
 
             if (self.zoneType == MapZone.ZoneType.OutOfBounds) {
+                StringBuilder sb = new($"{nameof(TeleportOutOfBoundsPickups)}> ");
                 // Try teleport to spawn location
                 if (other.TryGetComponent<OutOfBoundsPickupHelper>(out var helper)) {
-                    Vector3 position = helper.origin;
-                    TeleportHelper.TeleportGameObject(other.gameObject, position);
-                    if (other.attachedRigidbody) {
-                        other.attachedRigidbody.velocity = Vector3.zero;
+                    if (!helper.attempted) {
+                        Vector3 position = helper.origin;
+                        TeleportHelper.TeleportGameObject(other.gameObject, position);
+                        if (other.attachedRigidbody) {
+                            other.attachedRigidbody.velocity = Vector3.zero;
+                        }
+                        helper.attempted = true;
+                        sb.AppendLine($"Teleported pickup to {position}");
+                        sb.AppendLine($"\tentered {self.gameObject.name}");
+                        Plugin.Logger.LogDebug(sb.ToString());
                     }
-                    Object.Destroy(helper);
-                    Plugin.Logger.LogDebug($"{nameof(TeleportOutOfBoundsPickups)}> Teleported pickup to {position} [entered {self.gameObject.name}].");
+                    // Or nearest node to spawn location
+                    else {
+                        Vector3? position = TeleportToNearestNode(other);
+                        if (position.HasValue) {
+                            sb.AppendLine($"Teleported pickup to {position.Value}");
+                            sb.AppendLine($"\tentered {self.gameObject.name}");
+                            Plugin.Logger.LogDebug(sb.ToString());
+                        }
+                    }
                 }
-                // Fallback to nearest node
+                // Fallback to nearest node to current location
                 else {
                     bool hasPickupDropletController = other.TryGetComponent<PickupDropletController>(out _); // Droplet form
                     bool hasGenericPickupController = other.TryGetComponent<GenericPickupController>(out _); // Tangible form
                     bool hasPickupPickerController = other.TryGetComponent<PickupPickerController>(out _);   // Command cube form
+                    string debugComponent = $"{{ {nameof(hasPickupDropletController)}: {hasPickupDropletController}, {nameof(hasGenericPickupController)}: {hasGenericPickupController}, {nameof(hasPickupPickerController)}: {hasPickupPickerController} }}";
                     if (hasPickupDropletController || hasGenericPickupController || hasPickupPickerController) {
-                        // Logic from RoR2.Run.FindSafeTeleportPosition
-                        GameObject target = DirectorCore.instance.TrySpawnObject(new DirectorSpawnRequest(scTeleportHelper,
-                            new DirectorPlacementRule {
-                                placementMode = DirectorPlacementRule.PlacementMode.NearestNode,
-                                position = other.transform.position
-                            }, RoR2Application.rng));
-                        if (target != null) {
-                            Vector3 position = target.transform.position + (Vector3.up * other.bounds.extents.y);
-                            TeleportHelper.TeleportGameObject(other.gameObject, position);
-                            if (other.attachedRigidbody) {
-                                other.attachedRigidbody.velocity = Vector3.zero;
-                            }
-                            Object.Destroy(target);
-                            Plugin.Logger.LogDebug($"{nameof(TeleportOutOfBoundsPickups)}> Teleported pickup to {position} [entered {self.gameObject.name}] {{ {nameof(hasPickupDropletController)}: {hasPickupDropletController}, {nameof(hasGenericPickupController)}: {hasGenericPickupController}, {nameof(hasPickupPickerController)}: {hasPickupPickerController} }}.");
-                        } else Plugin.Logger.LogDebug($"{nameof(TeleportOutOfBoundsPickups)}> Failed to find a node to teleport to [entered {self.gameObject.name}].");
+                        Vector3? position = TeleportToNearestNode(other);
+                        if (position.HasValue) {
+                            sb.AppendLine($"Teleported pickup to {position.Value}");
+                            sb.AppendLine($"\tentered {self.gameObject.name}");
+                            sb.AppendLine($"\t{debugComponent}");
+                            Plugin.Logger.LogDebug(sb.ToString());
+                        }
+                        else {
+                            sb.AppendLine($"Failed to find a node to teleport to (from {other.transform.position})");
+                            sb.AppendLine($"\tentered {self.gameObject.name}");
+                            Plugin.Logger.LogDebug(sb.ToString());
+                        }
                     }
                 }
             }
         }
 
+        private static Vector3? TeleportToNearestNode(Collider other)
+        {
+            // Logic from RoR2.Run.FindSafeTeleportPosition
+            GameObject target = DirectorCore.instance.TrySpawnObject(new DirectorSpawnRequest(scTeleportHelper,
+                new DirectorPlacementRule {
+                    placementMode = DirectorPlacementRule.PlacementMode.NearestNode,
+                    position = other.transform.position
+                }, RoR2Application.rng));
+            if (target != null) {
+                Vector3 position = target.transform.position + (Vector3.up * other.bounds.extents.y);
+                TeleportHelper.TeleportGameObject(other.gameObject, position);
+                if (other.attachedRigidbody) {
+                    other.attachedRigidbody.velocity = Vector3.zero;
+                }
+                Object.Destroy(target);
+                return position;
+            }
+            return null;
+        }
+
         private class OutOfBoundsPickupHelper : MonoBehaviour
         {
             public Vector3 origin { get; private set; }
+            public bool attempted { get; internal set; }
 
             private void OnEnable()
             {
                 origin = transform.position;
+                attempted = false;
             }
         }
     }
